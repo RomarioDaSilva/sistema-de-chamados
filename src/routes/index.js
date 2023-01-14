9c5dc0202cc5f2d75b3c8ff1c33113d10054bd40
Romario <romario2656@gmail.com>
2023-01-14 12:23:29 -0300

    Criando pagina CADASTRO

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,17 @@ import { Routes, Route } from "react-router-dom";
 
 import SingIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import Dashboard from "../pages/Dashboard";
+
 
 function RoutsApp() {
   return (
     <Routes>
       <Route path="/" element={<SingIn />} />
       <Route path="/register" element={<SignUp />} />
+
+
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
